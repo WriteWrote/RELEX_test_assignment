3cbed60a121e04642eeff8bdb081a8950d3b8904
Элина Обухова <45429218+WriteWrote@users.noreply.github.com>
2023-02-22 21:48:15 +0300

    RunApp.class run

--- src/main/java/relex2023crypto/RunApp.java
+++ src/main/java/relex2023crypto/RunApp.java
@@ -1,5 +1,11 @@
 package relex2023crypto;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class RunApp {
+    public static void main(String[] args) {
+        SpringApplication.run(RunApp.class, args);
+    }
 }
